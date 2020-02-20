caec1b74526dce5bb8714619eb7469981e0c5011
fdokic <f.dokic@gmail.com>
2020-02-20 15:44:35 +0100

    extend input_completion to enable use of pytorch.onnx.export for use of deep500

--- deep500/tools/input_completion.py
+++ deep500/tools/input_completion.py
@@ -1,4 +1,4 @@
-from onnx import ModelProto, ValueInfoProto
+from onnx import ModelProto, ValueInfoProto, TensorShapeProto, TypeProto
 
 
 def complete_inputs(model: ModelProto):
@@ -11,6 +11,7 @@ def complete_inputs(model: ModelProto):
 
     missing_inputs = []
     inputs = model.graph.input
+    # complete inputs for initialization graph
     for training in model.training_info:
         bindings = training.initialization_binding
         for output in training.initialization.output:
@@ -20,6 +21,32 @@ def complete_inputs(model: ModelProto):
                 value.CopyFrom(output)
                 value.name = input_name
                 missing_inputs.append(value)
+
+    # comlete inputs for initializers
+    for i in model.graph.initializer:
+        if i.name in [j.name for j in model.graph.input]:
+            continue
+        type = i.data_type
+        shape = i.dims
+        name = i.name
+
+        tensor_type = TypeProto.Tensor()
+        type_proto = TypeProto()
+        shape_proto = TensorShapeProto()
+        for s in shape:
+            d = TensorShapeProto.Dimension()
+            d.dim_value = s
+            shape_proto.dim.extend([d])
+
+        tensor_type.shape.CopyFrom(shape_proto)
+        tensor_type.elem_type = type
+
+        type_proto.tensor_type.CopyFrom(tensor_type)
+
+        value = ValueInfoProto()
+        value.name = name
+        value.type.CopyFrom(type_proto)
+        missing_inputs.append(value)
 
     model.graph.input.extend(missing_inputs)
     return model
